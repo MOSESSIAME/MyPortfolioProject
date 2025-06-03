62f814b25e8a8e9fa3d9b0ce94b7235b50f62cea
Moses Siame <msiame1997@gmail.com>
2025-06-03 14:03:40 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// When page loads
+console.log("Portfolio page loaded successfully!");
+
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+  alert("Theme toggled!");
+}
